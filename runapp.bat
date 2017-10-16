bc940bd8286e27ff0a0f892e7ea3eab57a3cd2d3
shivaniguptaiam <32804762+shivaniguptaiam@users.noreply.github.com>
2017-10-15 22:26:04 -0700

    additional changes

--- runapp.bat
+++ runapp.bat
@@ -1,2 +1,11 @@
 call setenv.bat
-"%JAVA_HOME%"\bin\java -cp .\bin com.tokbox.bookstore.BookStoreMainApp
+@echo off
+if not exist ".\bin\com\tokbox\bookstore\BookStoreMainApp.class" (
+  
+  echo.
+  echo Please compile application first. Run compile.bat or ant
+  
+) else (
+  echo.
+  "%JAVA_HOME%"\bin\java -cp .\bin com.tokbox.bookstore.BookStoreMainApp
+)
